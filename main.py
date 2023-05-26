82951aa0fe48a91cd4c7d007a300bfa770adb5f8
prajwal sajjan <hcprajwal9901@gmail.com>
2023-05-26 10:13:56 +0530

    added map to all

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request,session
 import requests,sys
 
+
 app = Flask(__name__)
 rating=[]
 latitude=0
 longitude=0
 
-app.secret_key = 'gofuckyourself'
+
 @app.get('/')
 def home():
 
@@ -18,19 +19,22 @@ def process_form():
     global longitude
     global  latitude
     radius = request.form.get('radius')
-    
+    radiuskm = int(radius) * 1000
+    print(radiuskm)
     place_types = request.form.getlist('placeType')
     latitude=request.form.get('latitude')
     longitude=request.form.get('longitude')
+    print(latitude)
+    print(longitude)
     # Do something with the retrieved values, such as storing them in a database
     
     api_key = 'AIzaSyAfWP_iKshvMZmoUhz4VKXC4y3xoDTmE-4'
     if 'all' in place_types:
-        url = f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={latitude},{longitude}&radius={radius}&key={api_key}'
+        url = f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={latitude},{longitude}&radius={radiuskm}&key={api_key}'
     else:
         #place_type_str = ','.join(place_types)
          
-         url = f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={latitude},{longitude}&radius={radius}&keyword={place_types}&key={api_key}'
+         url = f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={latitude},{longitude}&radius={radiuskm}&keyword={place_types}&key={api_key}'
 
     #url = f'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={latitude},{longitude}&radius={radius}&keyword={place_type}&key={api_key}'
     
@@ -56,8 +60,8 @@ def process_form():
 
     rating+=top_rated_places
     for place in rating:
-        encoded_name = place['name'].encode(sys.stdout.encoding, errors='replace')
-        print(encoded_name.decode())
+          encoded_name = place['name'].encode('utf-8', errors='replace')
+          print(encoded_name.decode('utf-8', errors='replace'))
     
   
     return render_template('places.html', places=places)
@@ -67,6 +71,27 @@ def plan():
     
     return render_template('itinerary.html', top_rated_places=rating,user_latitude=latitude,user_longitude=longitude)
 
+@app.route('/place_info/<place_id>')
+
+def place_info(place_id):
+    def get_place_details(place_id):
+        api_key = 'AIzaSyAfWP_iKshvMZmoUhz4VKXC4y3xoDTmE-4'  # Replace with your own API key
+        url = f'https://maps.googleapis.com/maps/api/place/details/json'
+        params = {
+            'place_id': place_id,
+            'key': api_key
+        }
+        response = requests.get(url, params=params)
+        data = response.json()
+        place = data['result']
+        return place
+
+    place = get_place_details(place_id)
+    return render_template('place_info.html', place=place)
+
+
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0',port=8000)
 
